d1866c369eea0ae936098e763a4a6a12e55e318b
MERWAN SI_TAYEB <merwan.st@gmail.com>
2024-11-04 15:38:11 +0100

    initial commit

---
+++ src/main/java/com/merhene/edroms/EDromsApplication.java
@@ -0,0 +1,13 @@
+package com.merhene.edroms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EDromsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EDromsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/merhene/edroms/EDromsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.merhene.edroms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EDromsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
